c7ef3bb8b961991438033d67b7ba19b041c0482f
Josephfxw <Josephxwf@gmail.com>
2016-03-25 22:51:25 -0400

    add db

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ var express    = require('express'),
     storage    = require('./lib/store.js'),
     loadMiddleware    = require('./lib/load-middleware.js');
 
+    var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+
 // ***
 // *** OpenTok Constants for creating Session and Token values
 // ***
